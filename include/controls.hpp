e791c3ddab748c2e997a06d4fbedd569bc7c62cc
MagnusEvenstuen <magnusevenstuen777@gmail.com>
2023-11-07 18:54:46 +0100

    Endra måten skipet blei styrt på så det styrte med rotasjonen og ikke roterte utifra styringa

--- include/controls.hpp
+++ include/controls.hpp
@@ -6,6 +6,7 @@
 #include "lazarControls.hpp"
 #include <vector>
 #include <cmath>
+#include <iostream>
 
 using namespace threepp;
 namespace {
@@ -14,22 +15,29 @@ namespace {
     public:
         Controls(Object3D &obj, const std::shared_ptr<Scene>& scene) : obj_(obj), scene_(scene) {
         }
-
-        void onKeyPressed(const KeyEvent evt) override {
+        void onKeyPressed(const KeyEvent evt) override{
             if (evt.key == Key::W) {
-                speedY_ += 1;
-            }
-            if (evt.key == Key::S) {
-                speedY_ -= 1;
+                speed_ += 1;
             }
             if (evt.key == Key::A) {
-                speedX_ -= 1;
+                rotaition_ += M_PI/180;
             }
             if (evt.key == Key::D) {
-                speedX_ += 1;
+                rotaition_ -= M_PI/180;
             }
             if (evt.key == Key::SPACE) {
                 lazarControls_.createLasar(scene_, obj_);
+            }
+        }
+        void onKeyReleased(const KeyEvent evt) override {
+            if (evt.key == Key::W) {
+                speed_ -= 1;
+            }
+            if (evt.key == Key::A) {
+                rotaition_ -= M_PI/180;
+            }
+            if (evt.key == Key::D) {
+                rotaition_ += M_PI/180;
             }
         }
 
@@ -37,75 +45,29 @@ namespace {
             return lazarControls_.getLasars();
         }
 
-        void onKeyReleased(const KeyEvent evt) override {
-            if (evt.key == Key::W) {
-                speedY_ -= 1;
-            }
-            if (evt.key == Key::S) {
-                speedY_ += 1;
-            }
-            if (evt.key == Key::A) {
-                speedX_ += 1;
-            }
-            if (evt.key == Key::D) {
-                speedX_ -= 1;
-            }
-        }
-
         void setDeltaTime(const float dt) {
             dt_ = dt;
         }
 
         void setSpeed() {
-            if (speedY_ != 0 && speedX_ != 0) {
-                obj_.position.x += (speedMultiplier_ * speedX_ * dt_)/2;
-                obj_.position.y += (speedMultiplier_ * speedY_ * dt_)/2;
-                lazarControls_.setLasarSpeed(speedX_, speedY_);
-                float rotationAngle = atan2(speedY_, speedX_);
-                targetAngel_ = rotationAngle + M_PI/2;
-            } else if (speedY_ == 0 && speedX_ != 0 || speedY_ != 0 && speedX_ == 0) {
-                obj_.position.y += speedMultiplier_ * speedY_ * dt_;
-                obj_.position.x += speedMultiplier_ * speedX_ * dt_;
-                lazarControls_.setLasarSpeed(speedX_, speedY_);
-                float rotationAngle = atan2(speedY_, speedX_);
-                targetAngel_ = rotationAngle + M_PI/2;
-            }
-            float angleDiff = targetAngel_ - obj_.rotation.z();
-            if (angleDiff < -M_PI){
-                angleDiff += 2 * M_PI;
-            } else if (angleDiff > M_PI) {
-                angleDiff -= 2 * M_PI;
-            }
-
-            if (angleDiff < 0) {
-                obj_.rotation.z -= M_PI / 180.f * dt_ * 500;
-            } else if (angleDiff > 0) {
-                obj_.rotation.z += M_PI / 180.f * dt_ * 500;
-            }
+            obj_.rotation.z += rotaition_ * dt_ * 200;
+            Vector3 direction(cos(obj_.rotation.z()), sin(obj_.rotation.z()), 0);
+            obj_.position.x += speed_ * dt_ * speedMultiplier_ * direction[0];
+            obj_.position.y += speed_ * dt_ * speedMultiplier_ * direction[1];
             if (obj_.position.x > 30 || obj_.position.x < -30){
                 obj_.position.x *= -1;
-            }
-            if (obj_.position.y > 30 || obj_.position.y < -30){
+            } else if (obj_.position.y > 30 || obj_.position.y < -30){
                 obj_.position.y *= -1;
             }
-            if (!speed_.empty() && speedY_ != 0 || !speed_.empty() && speedX_ != 0) {
-                speed_.erase(speed_.begin());
-                speed_.emplace_back(speedX_, speedY_);
-            }
-            if (!speed_.empty() && speedY_ == 0 && speedX_ == 0) {
-                lazarControls_.setLasarSpeed(speed_[0].first, speed_[0].second);
-            }
-
+            lazarControls_.setLasarSpeed(direction[0], direction[1]);
             lazarControls_.updateLasars(scene_, dt_);
         }
 
     private:
         const float speedMultiplier_ = 10;
         float dt_{0};
-        float speedY_ = 0;
-        float speedX_ = 0;
-        float targetAngel_ = 0;
-        std::vector<std::pair<float, float>> speed_;
+        float speed_ = 0;
+        float rotaition_ = 0;
         Object3D &obj_;
         std::shared_ptr<Scene> scene_;
         std::vector<std::shared_ptr<Object>> lazars_;
